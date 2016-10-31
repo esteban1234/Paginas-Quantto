b2d77ffd4fb26354a9745e2da84803702e7d67c3
estebani1992 <tva_libra@hotmail.com>
2016-10-30 18:22:11 -0600

    nada

---
+++ Athlon/Vistas/ejemplo.php
@@ -0,0 +1,624 @@
+
+
+@media screen and (max-width: 480px){
+
+/*-----------------menuresponsivo------------*/
+
+.menudes {
+	/*border: 2px green solid;*/
+	width: 95%;
+	overflow: hidden;
+	padding: 0;
+	position: fixed;
+	height: auto;
+	border-radius: 0%;
+	margin-top: 0%;
+	left:-100%;
+	margin-left: 0%;
+}
+
+.menu
+{
+	display: block;
+	width: 100%;
+	background-color: #000;
+	color: white;
+	text-align: center;
+	cursor: pointer;
+	height: 50px;
+	font-size: 22px;
+	padding-top: 4%;
+}
+
+.menudes ul {
+	width: 100%;
+	/*line-height: 50px;*/
+	font-size: 20px;
+	text-align: left;
+	/*padding: 0px 0px 0px 60px;*/
+	/*color: #000;*/
+}
+.menudes ul li {
+	/*border: 2px white solid;*/
+	display: block;
+	width: 100%;
+	border-bottom:1px solid rgba(255,255,255, .9);
+	cursor: pointer;
+	/*padding: 0px 0px 0px 60px;*/
+
+
+}
+.menudes ul li a {
+	/*display: inline;*/
+ display: block;
+	/*width: 100%;*/
+
+}
+.marca {
+
+}
+
+
+
+.menudes ul li:nth-child(1):hover ~ .marca {
+left: -20%; /* 1/4 Ancho del enlace */
+}
+.menudes ul li:nth-child(2):hover ~ .marca {
+left: -50%;   
+}
+.menudes ul li:nth-child(3):hover ~ .marca {
+left: -60%;  
+}
+.menudes ul li:nth-child(4):hover ~ .marca {
+left: -80%;   
+}
+
+
+
+
+
+
+/*---------------------cuerpoindex-------------*/
+
+	.arriba{
+	/*border: 2px red solid;*/
+	width: 100%;
+	height: 20px;
+
+}
+
+.nombre{
+	/*border: 2px black solid;*/
+	width: 27%;	
+	margin-top: 0%;
+	margin-left: 1%;
+	font-size: 14px;
+	/*margin-left: 5%;*/
+}
+
+	.contactoarriba{
+	/*border: 2px red solid;*/
+	width: 46%;
+	margin-left:20%;
+	font-size: 14px;
+}
+
+header{
+	/*border: 2px red solid;*/
+	width: 100%;
+	height: 140px;
+	/*background-color: green;*/
+}
+
+.contenerdorimagen1{
+	/*border: 2px yellow solid;*/
+	width: 100%;
+	/*background-color: gray;*/
+}
+.figure1, .menudes{
+	display: block;
+	vertical-align: top;
+}
+.img1{
+	/*border: 2px red solid;*/
+	margin: 0 auto;
+	margin-left: 150%;
+	width:220%;
+}
+
+
+
+/*---------------------------sectionfotos--------------*/
+.contenedorfotos{
+	/*border: 2px red solid;*/
+	height: 400px;
+	width: 100%;
+}
+
+.bien{
+	/*border: 2px red solid;*/
+	font-size: 40px;
+	/*opacity: .5;*/
+	width: 87%;
+	margin-left: 6%;
+	margin-top: 35%;
+	text-align: center;
+/*color: rgba(29, 44, 44, 0.2);*/
+}
+.leer{
+	display: none;
+}
+
+.bienboton{
+	
+	 width: 48%;
+	 margin-left: 25%;
+}
+
+/*------------leyenda-----------*/
+.leyenda{
+	/*border: 2px red solid;*/
+	height: 100px;
+	width: 100%;
+}
+.petrus, .botonuno{
+	display: block;
+	vertical-align: top;
+}
+.petrus{
+	/*border: 2px black solid;*/
+	width: 100%;
+	font-size: 15px;
+	/*margin-top: 2.5%;*/
+	overflow: hidden;
+	margin-top: 1.5%;
+	margin-left: .5%;
+	text-align: center;
+ }
+ .botonuno{
+
+
+ 	width: 47%;
+ 	text-align: center;
+ 	margin-top: 2%;
+ 	margin-left: 28%;
+
+ }
+ /*--------------------------*/
+.contenedorcuadros{
+	/*border: 2px red solid;*/
+	width: 100%;
+}
+.biencontenedor{
+	font-size: 25px;
+	width: 90%;
+	margin-left: 3%;
+
+}
+.contendiseÃ±o, .contenampliaciones{
+	display: block;
+	vertical-align: top;
+padding: 1%;
+}
+
+.contenampliaciones{
+	/*background-color: gray;*/
+	margin-left: 8%;
+	/*border: 2px red solid;*/
+	width: 80%;
+	/*height: 400px;*/
+}
+.contendiseÃ±o{
+	/*background-color: gray;*/
+	margin-left: 8%;
+	/*border: 2px black solid;*/
+	width: 80%;
+	/*height: 400px;*/
+}
+.imgcons{
+    margin-left: 11%;
+}
+
+.botoncuadros{
+	/*border: 2px red solid;*/
+	width: 24%;
+	margin-top: 3%;
+	margin-bottom: 4%;
+}
+
+.solucion{
+	/*border: 2px red solid;*/
+	width: 100%;
+	height: 400px;
+
+}
+.biendos{
+	font-size: 22px;
+	width: 100%;
+	margin-left: 0%;
+	margin-top: 14%;
+	
+}
+
+.bientres{
+	font-size: 21px;
+	width: 100%;
+	margin-left: 0%;
+	margin-top: 5%;
+	margin-bottom: 5%;
+	
+}
+
+/*-----------footer---------*/
+
+footer{
+	width: 100%;
+}
+.piecondos{
+	/*border: 2px red solid;*/
+	/*text-align: center;*/
+	width: 100%;
+	margin-left: 0%;
+	margin-top: 1%;
+}
+.dtcdos{
+	font-size: 11px;
+	margin-left: 0%;
+	text-align: center;
+	
+}
+.petruspie{
+	font-size: 11px;
+	margin-top: 1%;
+	margin-left: 0%;
+	text-align: center;
+}
+
+
+/*-------------------------quienes somos-------------*/
+.contenedorfotosq{
+	/*border: 2px red solid;*/
+	height: 400px;
+	width: 100%;
+}
+#conq {
+    font-size: 25px;
+}
+.bienq{
+	font-size: 20px;
+	width: 100%;
+	margin-left: 0%;
+	margin-top: 2%;
+	text-align: center;
+}
+.bienqdos{
+	font-size: 20px;
+	margin-top: 5%;
+	width: 100%;
+	margin-left: 0%;
+	/*margin-top: 1%;*/
+	text-align: center;
+}
+#conq2 {
+    line-height: 40px;
+    margin-top: 8%;
+    margin-left: 0%;
+    font-size: 30px;
+    width: 100%;
+    text-align: center;
+ 
+ 
+}
+.contenedortextoq{
+	/*border: 2px red solid;*/
+	width: 100%;
+	margin: 0 auto;
+
+}
+.textoq, .imgq{
+	display: block;
+	vertical-align: top;
+}
+
+.textoq{
+	/*border: 2px black solid;*/
+	width: 100%;
+	text-align: center;
+
+}
+
+.textoqq{
+	/*border: 2px black solid;*/
+	width: 80%;
+	text-align: justify;
+	font-family: 'Dosis', sans-serif;
+	font-size: 14px;
+	margin: 0 auto;
+	
+}
+.imgq{
+	/*border: 2px red solid;*/
+	width: 100%;
+	margin-top: 5%;
+	margin-left: 0%;
+}
+.contenmision{
+	/*background-color: gray;*/
+	margin-left: 0%;
+	/*border: 2px red solid;*/
+	width: 90%;
+	/*height: 400px;*/
+	vertical-align: top;
+	margin: 0 auto;
+	display: block;
+}
+.botoncuadrosQ{
+	/*border: 2px red solid;*/
+	width: 50%;
+	font-size: 18px;
+	margin-left: 20%;
+	margin-top: 10%;
+}
+
+/*-------------------------servicios-------------*/
+.contenedorservicios{
+-moz-box-shadow: inset 0px 9px 19px -9px rgba(184,24,13,1);
+box-shadow: inset 0px 9px 19px -9px rgba(184,24,13,1);
+	width: 100%;
+	/*margin: 0 auto;*/
+	overflow: hidden;
+}
+#conse {
+    /*border: 2px green solid;*/
+    font-family: 'Oswald', sans-serif;
+    color: #505050;
+    /*outline-width: 40px;*/
+    line-height: 40px;
+    margin-top: 3%;
+    margin-left: 0%;
+    text-align: center;
+    font-size: 25px;
+ 
+ 
+}
+.servicios{
+	/*border: 2px black solid;*/
+	margin-bottom: 5%;
+	width: 100%;
+}
+
+.serv, .serv2{
+	display: block;
+	vertical-align: top;
+}
+
+.serv{
+	display: none;
+}
+#conserv{
+	width: 100%;
+	color: #b40022;
+	 /*font-family: 'Oswald', sans-serif;*/
+    /*color: #505050;*/
+    /*outline-width: 40px;*/
+    line-height: 30px;
+    /*margin-top: 2%;*/
+    margin-left: 0%;
+    font-size: 25px;
+   /*border: 2px red solid;*/
+   text-align: center;
+ 
+}
+.serv2{
+	/*border: 2px yellow solid;*/
+	margin-left: 0%;
+	font-family: 'Roboto', sans-serif;
+	text-align: justify;
+	color: #505050;
+	font-size: 16px;
+	margin: 0 auto;
+	margin-top: 5%;
+	width: 80%;
+
+	/*text-align: center;*/
+}
+
+.contenedorservicios2{
+	width: 100%;
+	/*border: 2px red solid;*/
+	/*text-align: center;*/
+	
+}
+.conteimagen{
+	/*border: 2px black solid;*/
+	/*text-align: center;*/
+  margin-top: 1%;
+}
+.textoo, .circulo, .iconosi{
+  display: block;
+  vertical-align: top;
+}
+
+.textoo{
+  margin-top: 9%;
+  /*margin-left: 0%;*/
+   margin-left: 6%;
+  width: 85%;
+  text-align: center;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size: 16px;
+  line-height: 22px;
+  /*border: 2px pink solid;*/
+  color: #505050;
+}
+
+.textoserv{
+	/*border: 2px yellow solid;*/
+	font-size: 16px;
+	/*border: 2px red solid;*/
+	margin-left: 0%;
+	margin: 0 auto;
+	/*font-family: helvetica;*/
+	/*color: #b40022;*/
+	color: #505050;
+	/*color: white;*/
+	 font-family: 'Oswald', sans-serif;
+	/*color: gray;*/
+	text-align: justify;
+	margin-top: 3%;
+	/*margin: 0 auto;*/
+	padding: 1px;
+
+}
+.circulo, .circulo2, .iconosii, .iconosi{
+	display: none;
+}
+
+.texto2{
+	/*border: 2px gray solid;*/
+  margin-top: 7%;
+  /*margin: 0 auto;*/
+  margin-left: 6%;
+  width: 85%;
+  text-align: justify;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-size: 16px;
+  line-height: 22px;
+  color: #505050;
+}
+.textodentro{
+	/*margin-top: 2%;*/
+	font-family: 'Oswald', sans-serif;
+	/*font-size: 16px;*/
+	/*color: #505050;*/
+	color: #b40022;
+	/*border: 2px green solid;*/
+	text-align: center;
+	/*color: black;*/
+	font-size: 20px;
+	/*text-align: center;*/
+	font-weight: bold;
+	margin-top: 1.5%;
+
+}
+/*-----------------contacto--------------*/
+
+.contactform{
+	/*border: 2px red solid;*/
+	overflow: hidden;
+	width: 95%;
+	margin: 0 auto;
+	margin-bottom: 4%;
+	
+}
+#con {
+    
+    font-size: 25px;
+ 
+ 
+}
+ 
+#te {
+	font-family: 'Oswald', sans-serif;
+    color: #505050;
+    width: 90%;
+    text-align: justify;
+    font-size: 16px;
+    margin-left: 0%;
+    margin-right: 0%;
+    margin: 0 auto;
+    /*border: 2px black solid;*/
+ 
+}
+.contenedorformdirec{
+	width: 95%;
+	margin-top: 2%;
+	/*border: 2px red solid;*/
+}
+
+#contacto{
+	width: 100%;
+}
+
+#contacto, .direc2{
+	display: block;
+	vertical-align: top;
+}
+
+#cote{
+	/*border: 2px red solid;*/
+}
+.emai{
+	/*margin-top: 0%;*/
+}
+.izq{
+	float: none;
+	width: 100%;
+	margin-left: 0%;
+	margin-right: 0%;
+}
+.der{
+	padding: 2% 0% 0% 0%;
+	float: none;
+	width: 100%;
+	margin-left: 0%;
+	margin-right: 0%;
+}
+.enviar{
+    /*border: 2px solid red;*/
+    font-size:16px;
+    font-weight:bold;
+    color:white;
+    background:#48a4ac;
+    padding: 15px;
+    border-radius: 5px;
+    width: 42%;
+    margin-bottom: 3%;
+    border:0px;
+    margin-left: 30%;
+    float: none;
+ 
+}
+.direc2{
+	/*border: 2px yellow solid;*/
+	margin-top: 5%;
+	margin-left: 0%;
+	width: 95%;
+	/*color: #b40022;*/
+}
+.direcp{
+	/*border: 2px red solid;*/
+	color: #505050;
+	font-size: 18px;
+	margin-top: 1%;
+	margin-left: 8%;
+	/*margin:0 auto;*/
+
+}
+
+.mapa{
+	width: 90%;
+	margin: 0 auto;
+	margin-top: 4%;
+	/*border: 2px red solid;*/
+}
+iframe{
+	width: 100%;
+}
+
+
+
+}
+
+@media screen and (max-width: 599px) and (min-width: 481px) {}
+
+@media screen and (max-width: 799px) and (min-width: 600px) {}
+
+@media screen and (max-width: 1023px) and (min-width: 800px) {}
+
+@media screen and (max-width: 1280px) and (min-width: 1024px) {      /* mac*/
+
+}
+@media screen and (max-width: 1366px) and (min-width: 1024px) { }
+
+@media screen and (min-width: 1367px) { }
