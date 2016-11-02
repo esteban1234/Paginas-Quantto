ee209dd83f8211ab4f2461471616f6fd3156b85d
esteban1234 <tva_libra@hotmail.com>
2016-11-02 15:56:24 -0600

    quienes somos

---
+++ Athlon/vistas/QuienesSomos.php
@@ -0,0 +1,138 @@
+<?php include 'head.php';?>
+
+
+	<!-- Header -->
+		<header>
+				<!-- Nav -->
+				<div class="nav-header">
+						<nav id="nav">
+							<ul>
+								<li><a href="Index.php"><span class="icon-home"></span>&nbsp;INICIO</a></li>
+								<li class="active"><a href="QuienesSomos.php"><span class="icon-users"></span>&nbsp;QUIENES SOMOS</a></li>
+								<li><a href="Servicios.php"><span class="icon-suitcase"></span>&nbsp;SERVICIOS</a></li>
+								<li><a href="Contacto.php"><span class="icon-old-phone"></span>&nbsp;CONTACTO</a></li>
+							</ul>
+						</nav>
+				</div>
+
+
+			<!-- Logo -->
+				<div id="logo">
+					<img class="athlon" src="../Img/ATHLON.png" alt="ATHLON PROFESIONALES EN ELECTRÓNICA">
+				</div>
+				<div id="header__icon" class="menu"><span class="icon-menu"></span></div>
+		</header>
+
+		<span class="ir-arriba icon-arrow-bold-up"></span>
+
+		<section class="conq">
+		<p class="conocenos">Conocenos</p>
+		<article class="informacioon">
+			<p class="textoinformativo">
+				<b>Athlon</b>, es una empresa 100% mexicana y orgullosamente yucateca, dedicada a la venta y consultoría de software. Somos una empresa especializada en desarrollo de soluciones integrales a problemas que requieren un software específico, contamos con un equipo de profesionales que desarrolla soluciones integrales con métodos que ayudan a nuestros clientes a confiar en las nuevas tecnologías y, al mismo tiempo, encontrar nuevos mercados para sus productos o servicios. <br><br>
+				Ofrecemos consultoría y soluciones tecnológicas innovadoras y eficaces que sean adaptables a las necesidades de nuestros clientes, fomentando su desarrollo y crecimiento, mediante un equipo de profesionales en tecnologías de la información altamente capacitados.
+				Nuestra experiencia nos ayuda a ofrecer soluciones que superan los objetivos y expectativas de nuestros clientes gracias a las estrategias y métodos que empleamos en cada proyecto.
+				 Somos una empresa especialista en consultoría de sistemas <b>ERP</b>. <br><br>
+				A la fecha Athlon se ha mantenido como líder en consultoría técnica y científica ofreciéndole una red amplia de soluciones garantizadas y rápidas, para evitar la pérdida de tiempo y dinero.
+			</p>
+		</article>
+		<article class="informaciondos"></article>
+		</section>
+
+
+		<section class="secciondosq">
+			<div class="main">
+	            <div class="view">
+	                <div class="mask">
+	                    <h2>MISIÓN</h2>
+	                    <p>Ofrecer un servicio de consultoría con la mayor actualización para nuestros usuarios poder desenvolverse satisfactoriamente en el área.</p>
+	                    <!-- <a href="#" class="info">Read More</a> -->
+	                </div>
+	            </div>
+	            <div class="view">
+	                <div class="mask">
+	                    <h2>VISIÓN</h2>
+	                    <p>Ser una empresa líder en consultoría desarrollando estrategias de negocios que apoyen en todo momento su crecimiento.</p>
+	                    <!-- <a href="#" class="info">Read More</a> -->
+	                </div>
+	            </div>
+	            <div class="view">
+	                <div class="mask">
+	                    <h2>OBJETIVOS</h2>
+	                    <p>Mejorar la productividad día a día ofreciendo productos y servicio de calidad, apoyándonos de un método de trabajo para satisfacer todos los requerimientos de nuestros clientes.</p>
+	                    <!-- <a href="#" class="info">Read More</a> -->
+	                </div>
+	            </div>
+	        </div>
+		</section>
+
+		<section class="valoresq">
+		<p class="nuestrosv">Nuestros Valores</p>
+			<article class="convalores">
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-cycle"></span>&nbsp;&nbsp;Responsabilidad
+					</p>
+				</article>
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-graduation-cap"></span>&nbsp;&nbsp;Calidad
+					</p>
+				</article>
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-emoji-happy"></span>&nbsp;&nbsp;Respeto
+					</p>
+				</article>
+
+			</article>
+
+			<article class="convalores">
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-briefcase"></span>&nbsp;&nbsp;Ética de trabajo
+					</p>
+				</article>
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-qq"></span>&nbsp;&nbsp;Compromiso
+					</p>
+				</article>
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-swap"></span>&nbsp;&nbsp;Equidad
+					</p>
+				</article>
+
+			</article>
+
+			<article class="convalores">
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-leaf"></span>&nbsp;&nbsp;Honestidad
+					</p>
+				</article>
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-picasa"></span>&nbsp;&nbsp;Autenticidad
+					</p>
+				</article>
+				<article class="valorestex">
+					<p>
+						<span class="iconov icon-star"></span>&nbsp;&nbsp;Liderazgo
+					</p>
+				</article>
+
+			</article>
+		</section>
+		<!-- <a href="Contacto.php" class="button" style="vertical-align:middle" ><span>Contáctanos</span></a> -->
+		<p class="contenbotonq"><a href="Contacto.php" class="buttonsecciondos" style="vertical-align:middle" ><span>Contáctanos</span></a></p>
+
+		<?php include 'Pie.php';?>
+
+		<script type="text/javascript" src="../js/jquery.js"></script>
+		<script src="../js/menu.js"></script>
+		<script type="text/javascript" src="../js/iconoarriba.js"></script>
+
+</body>
+</html>
